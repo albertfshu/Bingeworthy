1279fa6f451e61db9472f698917b6e736e9d9382
Dana Lelchuk <lelchukdana@gmail.com>
2023-07-18 15:42:19 -0500

    Redirect Sign out to Login page

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -8,6 +8,7 @@ const Nav = () => {
 
 const handleLogout = () => {
     logout()
+    window.location.href = './login';
 }
 
     return (
@@ -62,7 +63,7 @@ const handleLogout = () => {
                             <NavLink to={'/movielist'} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Movie List</NavLink>
                         </li>
                         <li>
-                            <NavLink to={'/'} onClick={handleLogout} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Sign Out</NavLink>
+                            <NavLink onClick={handleLogout} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Sign Out</NavLink>
                         </li>
                     </ul>
                 </div>
