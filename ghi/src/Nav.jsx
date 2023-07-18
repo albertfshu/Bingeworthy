fbc52c45359fe05acc53a1ac5519a4f9b2b848e0
yuni <jinjin.kyj@gmail.com>
2023-07-18 16:25:31 -0700

    updated nav bar

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -19,16 +19,13 @@ const Nav = () => {
 
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
                 <Link to="/" className="flex items-center">
                     <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Eo_circle_teal_letter-b.svg"
-                        className="h-8 mr-3"
+                        src="https://i.imgur.com/u34lKkt.png"
+                        className="h-12 w-200 mr-3"
                         alt="Bingeworthy Logo"
                     />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                        Bingeworthy
-                    </span>
                 </Link>
                 <div className="flex items-center md:order-2">
                     <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
@@ -62,9 +59,6 @@ const Nav = () => {
                 </div>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <NavLink to="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</NavLink>
-                        </li>
                         <li>
                             <NavLink to={'/movielist'} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Movie List</NavLink>
                         </li>
