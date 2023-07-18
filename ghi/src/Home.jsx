a332407eb25f962953776642e89f79333de58e76
Dana Lelchuk <lelchukdana@gmail.com>
2023-07-18 12:49:08 -0500

    Navbar

--- ghi/src/Home.jsx
+++ ghi/src/Home.jsx
@@ -1,9 +1,11 @@
 import Search from "./Search";
 import MovieList from "./Movielist";
+import Nav from "./Nav";
 
 const Home = () => {
   return (
     <>
+      <Nav />
       <Search />
       <MovieList />
     </>
