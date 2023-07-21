0101653910056ea5e266d89be2ed816f40afe9c0
Dana Lelchuk <lelchukdana@gmail.com>
2023-07-21 12:32:46 -0500

    Formatted date for Release Date

--- ghi/src/MovieDetail.jsx
+++ ghi/src/MovieDetail.jsx
@@ -8,22 +8,21 @@ import Reviews from "./Reviews";
 // import WatchlistButton from "./WatchlistButton"
 import MovieCard from "./Moviecard";
 
+
 const MovieDetail = () => {
     const { movie_id } = useParams();
     const { data: account } = useGetAccountQuery();
     const { data, isLoading } = useGetMovieDetailsQuery(movie_id);
 
+    if (isLoading) return <div> Loading... </div>;
 
-
-    // const FormattedDate = (dateString) => {
-    //     const options = { year: "numeric", month: "long", day: "numeric" };
-    //     return new Date(dateString).toLocaleDateString(undefined, options);
-
-    // };
-    // const ReleaseDate = FormattedDate(data.release_date);
+    const ReleaseDate = new Date(data.release_date).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
 
     //   implement watchlist function
-    if (isLoading) return <div> Loading... </div>;
     return (
         <div className="w-4/5 sm:w-full p-8 mx-auto">
             <div className="grid grid-cols-[200px,1fr] gap-8 object-center">
@@ -43,7 +42,7 @@ const MovieDetail = () => {
                     <h2 className="text-2xl">{data.original_title}</h2>
                     {/* <h5>rating</h5> */}
                     <div className="my-3">
-                        <h5 className="inline">{data.release_date} </h5>
+                        <h5 className="inline">{ReleaseDate} </h5>
                         <h5 className="inline pl-6">{data.runtime}min</h5>
                     </div>
                     <div>{data.overview}</div>
