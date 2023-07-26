36c2ed0a0f773def9563b6ef6a77584402fa185c
Dana Lelchuk <lelchukdana@gmail.com>
2023-07-26 17:44:13 -0500

    Unit tests for ratings

--- api/tests/test_ratings.py
+++ api/tests/test_ratings.py
@@ -1,45 +1,28 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries.accounts import AccountQueries
+from queries.ratings import RatingQueries
+from models import RatingIn, RatingOut
 
 client = TestClient(app)
 
+class FakeRatingQueries:
+    def create(self, info: RatingIn, page_id: str) -> RatingOut:
+        props = info.dict()
+        props["page_id"] = page_id
+        return RatingOut(**props)
 
-class FakeAccountQueries:
-    def get_all(self):
-        return [
-            {
-                "username": "test",
-            }
-        ]
 
-    def get_one_by_name(self):
-        return {
-            "username": "test",
-        }
+def test_create_rating():
+    app.dependency_overrides[RatingQueries] = FakeRatingQueries
+    page_id = 1
+    rating_in = RatingIn(value=4, user_id="1337")
+    res = client.post(f"/api/rating/{page_id}", json=rating_in.dict())
+    data = res.json()
+    print(data)
 
-    def test_get_all_accounts():
-        app.dependency_overrides[AccountQueries] = FakeAccountQueries
-
-        res = client.get("/api/accounts")
-        data = res.json()
-
-        assert res.status_code == 200
-        assert data == {
-            "accounts": [
-                {
-                    "username": "test",
-                }
-            ]
-        }
-
-    def get_account_by_username():
-        app.dependency_overrides[AccountQueries] = FakeAccountQueries
-
-        res = client.get("/api/accounts/test-accounts")
-        data = res.json()
-
-        assert res.status_code == 200
-        assert data == {
-            "username": "test",
-        }
+    assert res.status_code == 200
+    assert data == {
+        "page_id": "1",
+        "user_id": "1337",
+        "value": 4,
+    }
