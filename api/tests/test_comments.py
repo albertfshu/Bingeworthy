31e094d0f984eb862b1fa32bafee4979c3396981
Tai <jztai1@gmail.com>
2023-07-26 15:36:04 -0500

    Create comment test created.

--- api/tests/test_comments.py
+++ api/tests/test_comments.py
@@ -1,47 +1,36 @@
 from fastapi.testclient import TestClient
 from main import app
 from queries.comments import CommentQueries
+from models import Comments, CommentList, CommentIn
+from authenticator import authenticator
+import datetime
 
 client = TestClient(app)
 
 
 class FakeCommentQueries:
-    def get_all(self):
-        return [
-            {
-                "comment": "test",
-            }
-        ]
 
-    def get_one_by_name(self):
-        return {
-            "comment": "test",
-        }
+    def create(self, info: CommentIn, page_id: str) -> Comments:
+        props = info.dict()
+        props["page_id"] = page_id
+        props["post_date"] = datetime.datetime.now()
+        props["edit_date"] = props["post_date"]
+        return Comments(**props)
 
-    def test_get_all_accounts():
-        app.dependency_overrides[CommentQueries] = FakeCommentQueries
 
-        res = client.get("/api/comments")
-        data = res.json()
+def test_create_comment():
+    app.dependency_overrides[CommentQueries] = FakeCommentQueries
+    page_id = 1
+    comment_in = CommentIn(commentor_id="testuser", comment="testcomment")
+    res = client.post(f"/api/comments/{page_id}", json=comment_in.dict())
+    data = res.json()
+    print(data)
 
-        assert res.status_code == 200
-        assert data == {
-            "username": [
-                {
-                    "username": "test",
-                    "comment": "test",
-                    "comment_id": 1
-                }
-            ]
-        }
-
-    def get_account_by_username():
-        app.dependency_overrides[CommentQueries] = FakeCommentQueries
-
-        res = client.get("/api/comments/test-comments")
-        data = res.json()
-
-        assert res.status_code == 200
-        assert data == {
-            "comments": "test",
-        }
+    assert res.status_code == 200
+    assert data == {
+        "page_id": "1",
+        "commentor_id":"testuser",
+        "comment":"testcomment",
+        "post_date":data["post_date"],
+        "edit_date":data["edit_date"]
+    }
