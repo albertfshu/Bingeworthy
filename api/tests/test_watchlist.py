5c335f4376ce9101cf2106081602ff5c0badd28d
Albert Shu <albert94shu@gmail.com>
2023-07-26 14:23:39 -0500

    test_watchlist

--- api/tests/test_watchlist.py
+++ api/tests/test_watchlist.py
@@ -1,45 +1,84 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries.accounts import AccountQueries
+from queries.watchlist import WatchlistQueries
+from models import WatchlistIn
+from authenticator import authenticator
 
 client = TestClient(app)
 
 
-class FakeAccountQueries:
-    def get_all(self):
-        return [
-            {
-                "username": "test",
-            }
-        ]
+def fake_get_current_account_data():
+    return {"id": "1337", "username": "fakeuser"}
 
-    def get_one_by_name(self):
+
+
+class FakeWatchlistQueries:
+    def create(self, watchlist_in: WatchlistIn, account_id: str):
         return {
-            "username": "test",
+                "id": "watchlist_id",
+                "account_id": account_id,
+                "media_id": watchlist_in.media_id,
+                # or is it watchlist_in.media_id
         }
 
-    def test_get_all_accounts():
-        app.dependency_overrides[AccountQueries] = FakeAccountQueries
-
-        res = client.get("/api/accounts")
-        data = res.json()
-
-        assert res.status_code == 200
-        assert data == {
-            "accounts": [
+    def list_all_for_account(self, account_id: str):
+        return {
+            "watchlist": [
                 {
-                    "username": "test",
+                "id": "watchlist_id",
+                "account_id": account_id,
+                "media_id": "1",
                 }
             ]
         }
 
-    def get_account_by_username():
-        app.dependency_overrides[AccountQueries] = FakeAccountQueries
+    def delete(self, watchlist_id: str, account_id: str):
+        return True
 
-        res = client.get("/api/accounts/test-accounts")
-        data = res.json()
 
-        assert res.status_code == 200
-        assert data == {
-            "username": "test",
+def test_list_watchlist_for_account():
+    app.dependency_overrides[WatchlistQueries] = FakeWatchlistQueries
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
+    res = client.get("/api/accounts/{account_id}/watchlist")
+    data = res.json()
+    print(data)
+
+    assert res.status_code == 200
+    assert "watchlist" in data
+    assert data["watchlist"] == [
+        {
+            "id": "watchlist_id",
+            "account_id": "1337",
+            "media_id": "1",
         }
+
+    ]
+
+    print(res, res.status_code, data)
+
+
+def test_create_watchlist():
+    app.dependency_overrides[WatchlistQueries] = FakeWatchlistQueries
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
+
+    watchlist_in = WatchlistIn(media_id="1")
+
+    res = client.post("/api/accounts/{account_id}/watchlist", json=watchlist_in.dict())
+    data = res.json()
+
+    assert res.status_code == 200
+
+    assert data["account_id"] == "1337"
+    assert data["media_id"] == "1"
+
+
+def test_delete_watchlist():
+    app.dependency_overrides[WatchlistQueries] = FakeWatchlistQueries
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
+
+    # watchlist_id = "1"
+    res = client.delete("/api/accounts/{account_id}/watchlist/{watchlist_id}")
+    data = res.json()
+
+    assert res.status_code == 200
+    assert data == {'success': True}
